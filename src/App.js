bbdc54297194b5a958174f200336bcc5082e7c93
Nav9Nav123 <155417167+Nav9Nav123@users.noreply.github.com>
2024-01-31 20:10:33 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Codin 1</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
